cbd80b738c8122b63924a8b058c9a1d92cbe98a7
vageesh pandith <vageshradhya@gmail.com>
2024-09-02 11:40:00 +0530

    Add data processing logic to main

---
+++ src/main.c
@@ -0,0 +1,12 @@
+// src/main.c
+#include "sensor.h"
+void process_data(int data) {
+    // Code to process data
+}
+
+int main() {
+    int data = read_sensor_data();
+    process_data(data);
+    return 0;
+}
+
