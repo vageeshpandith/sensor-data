599337095d159069e612a8b41efdfe9e3ca55c49
vageesh pandith <vageshradhya@gmail.com>
2024-09-02 11:31:41 +0530

    Implement sensor init and reading the data

---
+++ src/sensor.c
@@ -0,0 +1,10 @@
+// src/sensor.c
+void init_sensor() {
+    // Code to initialize sensor
+}
+
+int read_sensor_data() {
+    // Code to read sensor data
+    return 0; // Placeholder
+}
+
---
+++ src/source.h
@@ -0,0 +1,2 @@
+read_sensor_data();
+
